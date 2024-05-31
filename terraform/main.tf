f528b4171002671ff47c2eadb73bc8af3a08b40b
Hallexz <yevtefeevah@gmail.com>
2024-05-31 20:55:59 +0700

    Merge remote-tracking branch 'master/master'

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,44 @@
 variable "yandex_cloud_token" {
   description = "Yandex Cloud API token"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "network_name" {
   description = "Network name"
-  type = string
-  default = "my-network"
+  type        = string
+  default     = "my-network"
 }
-
 
 variable "subnet_name" {
   description = "Subnet name"
-  type = string
-  default = "my-subnet"
+  type        = string
+  default     = "my-subnet"
 }
-
 
 variable "zone" {
   description = "Availability zone"
-  type = string
-  default = "ru-central1-b"
+  type        = string
+  default     = "ru-central1-b"
 }
-
 
 terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-      version = "0.83.0"
+      version = "0.83.0" # Используйте последнюю доступную версию
     }
   }
 }
 
+provider "yandex" {
+  token     = var.yandex_cloud_token
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
+  zone      = var.zone
+}
 
-resource "yandex_network" "default" {
+resource "yandex_vpc_network" "default" {
   name = var.network_name
   description = "Пример сети"
   labels = {
@@ -43,15 +46,11 @@ resource "yandex_network" "default" {
   }
 }
 
-
-resource "yandex_network_subnet" "default" {
-  name = var.subnet_name
-  network_id = yandex_network.default.id
-  zone = var.zone
-  ip_version = "ipv4"
-  address_space {
-    cidr_blocks = ["10.128.0.0/20"]
-  }
+resource "yandex_vpc_subnet" "default" {
+  name           = var.subnet_name
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.128.0.0/20"]
+  zone           = var.zone
 }
 
 data "yandex_compute_image" "ubuntu" {
@@ -59,19 +58,12 @@ data "yandex_compute_image" "ubuntu" {
 }
 
 resource "yandex_compute_instance" "default" {
-  name = "my-instance"
-  zone = var.zone
+  name        = "my-instance"
   platform_id = "standard-v1"
+
   resources {
     cores  = 2
     memory = 2
-  }
-  network_interface {
-    network_id = yandex_network.default.id
-    subnet_id = yandex_network_subnet.default.id
-    nat_ip {
-      address_type = "external"
-    }
   }
 
   boot_disk {
@@ -80,5 +72,14 @@ resource "yandex_compute_instance" "default" {
       type     = "network-hdd"
       size     = 10
     }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default.id
+    nat       = true
+  }
+
+  metadata = {
+    foo = "bar"
   }
 }
---
+++ terraform/main.tfvars
@@ -0,0 +1,3 @@
+yandex_cloud_token = "${{ secrets.YANDEX_CLOUD_TOKEN }}"
+yandex_cloud_id = "${{ secrets.YANDEX_CLOUD_ID  }}"
+yandex_folder_id= "${{ secrets.YANDEX_FOLDER_ID }}"
